3f0f01b065423677c22c8630688d0b105cc6ffe2
harrynurbudyana <harrynurbudyana@gmail.com>
2024-12-11 21:54:57 +0700

    update

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,14 @@
 #include <Arduino.h>
+#include <HTTPClient.h>
+#include <WiFi.h>
+#include <Wire.h>
+
+const char* ssid     = "this.wifi";
+const char* password = "this@wifi";
+
+// REPLACE with your Domain name and URL path or IP address with path
+const char* serverName = "http://20.189.125.134:9000/product";
+
 
 int ph0 = A0;
 int samples = 10;
@@ -10,9 +20,18 @@ int myFunction(int, int);
 void setup() {
   // put your setup code here, to run once:
   int result = myFunction(2, 3);
-  Serial.begin(9600);
+  Serial.begin(115200);
   delay(1000);
-  Serial.println('pH sense');
+  Serial.println("pH sense");
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting");
+  while(WiFi.status() != WL_CONNECTED) { 
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.print("Connected to WiFi network with IP Address: ");
+  Serial.println(WiFi.localIP());
   }
 
 float pH(float voltage){
@@ -30,6 +49,29 @@ void loop() {
   Serial.print("pH = ");
   Serial.println(pH(voltage));
   delay(3000);
+  if(WiFi.status() == WL_CONNECTED){
+    HTTPClient http;
+
+    http.begin(serverName);
+    http.addHeader("Content-Type", "application/json");
+    int httpResponseCode = http.POST("{\"name\":\"string\",\"price\":7000,\"description\":\"string\"}");
+    if(httpResponseCode > 0){
+      Serial.print("HTTP Response Code: ");
+      Serial.println(httpResponseCode);
+      if (httpResponseCode == 200 || httpResponseCode == 201)
+      {
+        Serial.println("Data berhasil dikirim");
+      }
+      
+    } else {
+      Serial.print("Error Code: ");
+      Serial.println(httpResponseCode);
+    }
+    http.end();
+  } else {
+    Serial.println("WiFi Disconnected");
+  }
+  
 }
 
 // put function definitions here:
